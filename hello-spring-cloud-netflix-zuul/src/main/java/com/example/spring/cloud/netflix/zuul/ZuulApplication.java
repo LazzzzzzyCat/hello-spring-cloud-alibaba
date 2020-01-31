0cad8ea67f19de34c5377259c3b59320e7f653b7
Messi <470661893@qq.com>
2020-01-31 12:51:11 +0800

    commit

---
+++ hello-spring-cloud-netflix-zuul/src/main/java/com/example/spring/cloud/netflix/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.example.spring.cloud.netflix.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * zuul demo
+ * @author huwj
+ * @date 2020-01-31 11:24
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
