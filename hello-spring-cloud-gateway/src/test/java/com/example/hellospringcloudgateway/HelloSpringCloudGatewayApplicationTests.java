911fda288ea7fd01f9bb27081f5ae44c2d651ec0
Messi <470661893@qq.com>
2020-01-31 12:50:23 +0800

    commit

---
+++ hello-spring-cloud-gateway/src/test/java/com/example/hellospringcloudgateway/HelloSpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringcloudgateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
